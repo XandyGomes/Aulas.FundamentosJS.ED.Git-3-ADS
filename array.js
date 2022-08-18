2f9e405a97774c9314d83b3bccadf91e5267a0a0
XandyGomes <x3.computadores@hotmail.com>
2022-08-18 20:41:26 -0300

    aula.18.08.2022

---
+++ array.js
@@ -0,0 +1,15 @@
+const valores = [7, 10, 20, 35, 50, 75, 80]
+// console.log(valores)
+
+// console.log("o valor da posição 3 é: " + valores[3])
+
+console.log(valores.length)
+
+valores[7] = 100
+console.log(valores)
+
+console.log(valores.pop())
+console.log(valores)
+
+delete valores[2]
+console.log(valores)
